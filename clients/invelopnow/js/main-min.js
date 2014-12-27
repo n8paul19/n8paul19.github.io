7acf4874ba39e38dfee4483912ba68d5f632a73e
Nate Paul <nate.paul@cph.org>
2014-12-27 15:38:06 -0600

    update to many things

--- clients/invelopnow/js/main-min.js
+++ clients/invelopnow/js/main-min.js
@@ -2258,3 +2258,44 @@ $(function(){
     });
 });
 
+$(function(){
+  var onClass = "on";
+  var showClass = "show";
+  
+  $("input").bind("checkval",function(){
+    var label = $(this).prev("label");
+    if(this.value !== ""){
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on("keyup",function(){
+    $(this).trigger("checkval");
+  }).on("focus",function(){
+    $(this).prev("label").addClass(onClass);
+  }).on("blur",function(){
+      $(this).prev("label").removeClass(onClass);
+  }).trigger("checkval");
+});
+
+$(function(){
+  var onClass = "on";
+  var showClass = "show";
+  
+  $("textarea").bind("checkval",function(){
+    var label = $(this).prev("label");
+    if(this.value !== ""){
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on("keyup",function(){
+    $(this).trigger("checkval");
+  }).on("focus",function(){
+    $(this).prev("label").addClass(onClass);
+  }).on("blur",function(){
+      $(this).prev("label").removeClass(onClass);
+  }).trigger("checkval");
+});
+
+
