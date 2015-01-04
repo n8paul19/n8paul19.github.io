8c4c8ea93cfc61ba5df0019cf2eb1b4c55a02b3a
Nate Paul <nate.paul@cph.org>
2015-01-04 15:15:16 -0600

    Updated contact thank you page

--- clients/invelopnow/contactthanks.php
+++ clients/invelopnow/contactthanks.php
@@ -1,28 +1,132 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Thank You! - Invelop, 3 Dimensional Solutions</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-	<title>A Nice & Simple Contact Form</title>
-	
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/modernizr.custom.js"></script>
 </head>
-
 <body>
+    <!--[if lt IE 7]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div class="container">
+    <!-- header -->
+    <header>
+      <div class="wrapper">
+        <a class="nav-logo" href="index.html">
+            <video class="logo-loop" width="480" height="100" autoplay="autoplay" preload="none"poster="images/invelop-logo.jpg" loop>
+              <source src="images/invelop-logo.mov" type="video/mov" />
+              <source src="images/invelop-logo.ogg" type="video/ogg" />
+              <img src="images/invelop-logo.jpg" class="logo">
+            </video>
+          </a>
+        <div class="social">
+            <a href="https://www.facebook.com/pages/Invelop-LLC/778989742148951?skip_nax_wizard=true&ref_type=logout_gear" target="_blank"><span class="icon-facebook"></span></a>
+            <a href="https://twitter.com/invelopnow" target="_blank"><span class="icon-twitter"></span></a>
+            <a href="https://plus.google.com/u/0/117870942033177453042/about" target="_blank"><span class="icon-googleplus"></span></a>
+            <a href="https://www.youtube.com/channel/UCLYH2GIenP3pVFECHDcP-hg" target="_blank"><span class="icon-youtube"></span></a>
+            <a href=""><span class="icon-linkedin"></span></a>
+            <a href="http://instagram.com/invelopnow?ref=badge" target="_blank"><span class="icon-instagram"></span></a>
+            <a href="http://www.pinterest.com/invelopnow/"><span class="icon-pinterest" target="_blank"></span></a>
+          </div>
+        <nav>
+         <a href="services.html">Services</a>
+          <a href="about.html">About</a>
+          <a href="#" id="trigger-overlay">Contact Us</a>
+        </nav>
+      </div>
+    </header>
+    <!-- /header -->
+    <section class="about">
+      <div class="alpha">
+          <h1>Thank you!</h1>
+        </div>
+      <div class="wrapper">
+        
+        <div class="gamma">
 
-	<div id="page-wrap">
+        <p class="lead">We've recieved your message and will be in touch right away.</p></div>
+      </div>
+    </section>
+<!-- FOOTER -->
+<footer>
+  <div class="email-signup">
+    <span class="icon-mail"></span>
+    <h3>Sign up for Invelop’s Newsletter</h3>
+    <div class="field-wrapper">
+      <label for="fname">You're gonna love it!</label>
+      <input name="fname" placeholder="Enter your email address " />
+    </div>
+    <p>We don’t share email addresses and you can unsubscribe at any time.</p>
+  </div>
+  <div class="footer-bottom">
+    <p>@2015 Invelop LLC •  All Rights Reserved</p>
+  </div>
+</footer>
+</div>
+<!-- Overlay -->
+<div class="overlay overlay-contentscale">
+  <button type="button" class="overlay-close">Close</button>
+  
+  <section class="contact-form">
+    <div class="wrapper">
+      <h2>Contact Us</h2>
+      <div class="contact-phone">
+       <span class="icon-mobile"></span> 314.555.5555
+     </div>
+     <div class="contact-social">
+     </div>
+     
+     <form>
+      <feildset>
+        <div class="field-wrapper">
+          <label for="fname">Your Name</label>
+          <input name="fname" placeholder="First and Last Name " />
+        </div>
 
-		<img src="images/title.gif" alt="A Nice & Simple Contact Form" />
-		<p>By <a href="http://css-tricks.com">CSS-Tricks</a></p>
-			
-			
-		<br /><br />
-		
-			
-		<h1>Your message has been sent!</h1><br />
-		
-		<p><a href="index.html">Back to Contact Form</a></p>
-	
-	</div>
+        <div class="field-wrapper">
+          <label for="lname">Your Email</label>
+          <input name="email" placeholder="Email" />
+        </div>
 
-</body>
+        <div class="field-wrapper">
+          <label for="phone">Your Number</label>
+          <input name="phone" placeholder="Phone Number" />
+        </div>
 
+        <div class="field-wrapper">
+          <label for="message">Your Message</label>
+          <textarea rows="4" cols="50"input name="message" placeholder="Send us a note"></textarea>
+        </div>
+        <a href="" class="btn btn-primary btn-contact">Send</a>
+      </feildset>
+    </form>
+  </div>
+  
+</section>
+</div>
+</div><!-- /container -->
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="js/main-min.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+  </body>
 </html>
