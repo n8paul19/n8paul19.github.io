ec8e322d5d5e3ef97babc632eb879f69bc38a1f0
Nate Paul <nate.paul@cph.org>
2015-04-25 08:39:01 -0500

    [clients / invelopnow]

---
+++ clients/invelopnow-production/contactthanks.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Thank You! - Invelop, 3D Solutions</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="apple-touch-icon-precomposed" sizes="57x57" href="apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="60x60" href="apple-touch-icon-60x60.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="120x120" href="apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="76x76" href="apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
+  <link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+  <link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+  <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+  <link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+  <meta name="application-name" content="&nbsp;"/>
+  <meta name="msapplication-TileColor" content="#FFFFFF" />
+  <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+  <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+  <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+  <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+  <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="js/modernizr.custom.js"></script>
+</head>
+<body>
+    <!--[if lt IE 7]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <div class="container">
+    <!-- header -->
+    <header>
+      <div class="wrapper">
+        <a class="nav-logo" href="/">
+            <video class="logo-loop" width="480" height="100" autoplay="autoplay" preload="none"poster="images/invelop-logo.jpg" loop>
+              <source src="images/invelop-logo.mov" type="video/mov" />
+              <source src="images/invelop-logo.ogg" type="video/ogg" />
+              <img src="images/invelop-logo.jpg" class="logo">
+            </video>
+          </a>
+        <div class="social">
+            <a href="https://www.facebook.com/pages/Invelop-LLC/778989742148951?skip_nax_wizard=true&ref_type=logout_gear" target="_blank"><span class="icon-facebook"></span></a>
+            <a href="https://twitter.com/invelopnow" target="_blank"><span class="icon-twitter"></span></a>
+            <a href="https://plus.google.com/u/0/117870942033177453042/about" target="_blank"><span class="icon-googleplus"></span></a>
+            <a href="https://www.youtube.com/channel/UCLYH2GIenP3pVFECHDcP-hg" target="_blank"><span class="icon-youtube"></span></a>
+            <a href="https://www.linkedin.com/company/9219645?trk=tyah&trkInfo=tarId%3A1421164454460%2Ctas%3Ainvelop%20llc%2Cidx%3A1-1-1" target="_blank"><span class="icon-linkedin"></span></a>
+            <a href="http://instagram.com/invelopnow?ref=badge" target="_blank"><span class="icon-instagram"></span></a>
+            <a href="http://www.pinterest.com/invelopnow/"><span class="icon-pinterest" target="_blank"></span></a>
+          </div>
+        <nav>
+         <a href="services.html">Services</a>
+          <a href="faqs.html">FAQs</a>
+          <a href="#" id="trigger-overlay">Contact Us</a>
+        </nav>
+      </div>
+    </header>
+    <!-- /header -->
+    <section class="about">
+      <div class="alpha">
+          <h1>Thank you!</h1>
+        </div>
+      <div class="wrapper">
+        
+        <div class="gamma">
+
+        <p class="lead">We've received your message and will be in touch right away.</p></div>
+      </div>
+    </section>
+<!-- FOOTER -->
+<footer>
+  <div id="mc_embed_signup" class="email-signup">
+    <form action="//invelopnow.us9.list-manage.com/subscribe/post?u=778993d3c4f211e0340bdcb71&amp;id=1f07ac002b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <span class="icon-mail"></span>
+      <h3>Sign up for Invelop’s Newsletter</h3>
+      <div id="mc_embed_signup_scroll" class="field-wrapper">
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Enter your email address" required>
+        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;"><input type="text" name="b_778993d3c4f211e0340bdcb71_1f07ac002b" tabindex="-1" value=""></div>
+        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn"></div>
+        <p>We don’t share email addresses and you can unsubscribe at any time.</p>
+      </div>
+    </form>
+  </div>
+  <div class="footer-bottom">
+    <p>@2015 Invelop LLC •  All Rights Reserved</p>
+  </div>
+</footer>
+</div>
+<!-- Overlay -->
+<div class="overlay overlay-contentscale">
+  <button type="button" class="overlay-close">Close</button>
+
+  <section class="contact-form">
+    <div class="wrapper">
+      <h2>Contact Us</h2>
+      <div class="contact-phone">
+	<img src="https://service.ringcentral.com/picture/dot.gif" width="1" height="1"/><div style="cursor:pointer; display:inline-block;" onclick='var wind = window; var winop = wind.open; winop("https://service.ringcentral.com/ringme/?uc=860FB34D85E0BEBA57F5209C9DEF66D8437057569959,0,202,1,0&s=no&v=2&s_=1210", "Callback_RingMe", "resizable=no,width=650,height=460"); return false;'><span class="icon-mobile"></span> RingMe Now</div>
+</div>
+
+     <form method="post" action="contactengine.php">
+      <feildset>
+        <div class="field-wrapper">
+          <label for="Name">Your Name</label>
+          <input name="Name" id="Name" placeholder="First and Last Name " />
+        </div>
+
+        <div class="field-wrapper">
+          <label for="Tel">Your Number</label>
+          <input name="Tel" id="Tel" placeholder="Phone Number" />
+        </div>
+
+        <div class="field-wrapper">
+          <label for="Email">Your Email</label>
+          <input name="Email" id="Email" placeholder="Email" />
+        </div>
+
+        <div class="field-wrapper">
+          <label for="Message">Your Message</label>
+          <textarea rows="4" cols="50"input name="Message" id="Message" placeholder="Send us a note"></textarea>
+        </div>
+        
+        <button type="submit" class="btn submit-button">
+          <span class="icon-paperplane"></span> Send
+        </button>
+      </feildset>
+    </form>
+  </div>
+
+</section>
+</div>
+</div><!-- /container -->
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<script src="js/main-min.js"></script>
+
+     
+    <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='//www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-58307127-1');ga('send','pageview');
+    </script>
+  </body>
+</html>
---
+++ clients/invelopnow-production/js/main.js
@@ -0,0 +1,54 @@
+$(function(){
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - $("header").outerHeight(true);
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+$(function(){
+  var onClass = "on";
+  var showClass = "show";
+  
+  $("input").bind("checkval",function(){
+    var label = $(this).prev("label");
+    if(this.value !== ""){
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on("keyup",function(){
+    $(this).trigger("checkval");
+  }).on("focus",function(){
+    $(this).prev("label").addClass(onClass);
+  }).on("blur",function(){
+      $(this).prev("label").removeClass(onClass);
+  }).trigger("checkval");
+});
+
+$(function(){
+  var onClass = "on";
+  var showClass = "show";
+  
+  $("textarea").bind("checkval",function(){
+    var label = $(this).prev("label");
+    if(this.value !== ""){
+      label.addClass(showClass);
+    } else {
+      label.removeClass(showClass);
+    }
+  }).on("keyup",function(){
+    $(this).trigger("checkval");
+  }).on("focus",function(){
+    $(this).prev("label").addClass(onClass);
+  }).on("blur",function(){
+      $(this).prev("label").removeClass(onClass);
+  }).trigger("checkval");
+});
